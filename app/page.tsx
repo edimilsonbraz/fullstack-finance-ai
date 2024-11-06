33c708b1a6adf7daebc3e41c834e2daa9e7c7e8c
edimilsonbraz <edimilson.gt8@gmail.com>
2024-11-06 15:50:03 -0300

    :wrench: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
-    <div className="w-screen items-center justify-center">
-      <h1 className="p-5 font-bold text-red-500">Hello world</h1>
+    <div className="flex w-screen items-center justify-center">
+     <Button>Hello world</Button>
     </div>
   );
 }
