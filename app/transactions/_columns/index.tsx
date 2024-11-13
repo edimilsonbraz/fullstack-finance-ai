71205e5d4b30797db741405a7280d1c9a6e57a50
edimilsonbraz <edimilson.gt8@gmail.com>
2024-11-13 16:12:11 -0300

    :sparkles: add table transaction

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { CircleIcon } from "lucide-react";
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => {
+      if (transaction.type === TransactionType.DEPOSIT) {
+        return (
+          <Badge className="bg-muted text-primary hover:bg-muted">
+            <CircleIcon className="mr-2 fill-primary" size={10} />
+            Depósito
+          </Badge>
+        );
+      }
+      if (transaction.type === TransactionType.EXPENSE) {
+        return "Despesa";
+      }
+      return "Investimento";
+    },
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,26 @@
-const TransactionsPage = () => {
-  return <h1>Transactions page</h1>
-}
+import { ArrowDownUp } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  // Acessar as transações do banco de dados
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="space-y-6 p-6">
+      {/* TITULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUp />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
+};
 
 export default TransactionsPage;
